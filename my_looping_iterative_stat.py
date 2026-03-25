ebe955d260dbd9848902e8ff1e5bcc501f7c5a2e
unknown <shubhamnarware67@gmail.com>
2026-03-25 20:10:07 +0530

    Update looping iterative statements

--- my_looping_iterative_stat.py
+++ my_looping_iterative_stat.py
@@ -162,7 +162,4 @@ while i<n:
         l.append(i)
     i=i+1
 print(f'Factor of given number {n} is {l}')'''
- # 
-    print(f'Given number {p} is not armstrong')
-
-            
+ # print(f'Given number {p} is not armstrong')
